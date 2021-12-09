d6156c2e70ad27821b42d47ad70c8eb6beadb6c6
cxh <chenxinhao@tal.com>
2021-12-09 09:52:25 +0800

    整理setState

--- src/setState/index.js
+++ src/setState/index.js
@@ -18,16 +18,18 @@
 //   next: null
 // }
 
+// 首次render时是mount
+let isMount = true;
+// 指向当前操作的hook对象
+let workInProgressHook = null;
+
 // App组件对应的fiber对象
 const fiber = {
-  // 保存该FunctionComponent对应的Hooks链表
-  memoizedState: null, // 多个useState的hook对象组成一个单向无环链表
+  // 多个useState的hook对象组成一个单向无环链表
+  memoizedState: null, // 指向第一个hook
   // 指向App函数
   stateNode: App
 };
-
-// 首次render时是mount
-isMount = true;
 
 function schedule() {
   // 更新前将workInProgressHook重置为fiber保存的第一个Hook
@@ -60,8 +62,6 @@ function dispatchAction(queue, action) {
   schedule();
 }
 
-let workInProgressHook = null; // 当前指向的hook对象
-
 const useState = (initialState) => {
   // 当前useState使用的hook会被赋值该该变量
   let hook;
@@ -75,13 +75,14 @@ const useState = (initialState) => {
       memoizedState: initialState,
       next: null
     }
-    // 将hook插入fiber.memoizedState链表末尾
     if (!fiber.memoizedState) {
+      // 将第一个hook插入fiber.memoizedState链表
       fiber.memoizedState = hook;
     } else {
+      // 把下一个hook链接到的上一个hook的next上
       workInProgressHook.next = hook;
     }
-    // 移动workInProgressHook指针（等于移到链表末尾）
+    // 移动workInProgressHook指针（等于移到hook链表末尾）
     workInProgressHook = hook;
   } else {
     // update时从workInProgressHook中取出该useState对应的hook
@@ -114,13 +115,18 @@ const useState = (initialState) => {
 
 function App() {
   const [num, updateNum] = useState(0);
+  const [num1, updateNum1] = useState(1);
 
   console.log(`${isMount ? 'mount' : 'update'} num: `, num);
+  console.log(`${isMount ? 'mount1' : 'update1'} num1: `, num1);
 
   // return <p onClick={() => updateNum(num => num + 1)}>{num}</p>;
   return({
     click() {
       updateNum(num => num + 1);
+    },
+    click1() {
+      updateNum1(num => num + 1);
     }
   })
 }
